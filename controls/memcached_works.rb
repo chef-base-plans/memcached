a78f83de1627305bc30a88a0fa5b5b1df8cbfeb5
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-09 10:39:49 +0100

    Ensure pkg_installation_directory is verified in each test before proceeding
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/memcached_works.rb
+++ controls/memcached_works.rb
@@ -2,9 +2,6 @@ title 'Tests to confirm memcached works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'memcached')
-plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
-plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
 control 'core-plans-memcached-works' do
   impact 1.0
@@ -14,11 +11,14 @@ control 'core-plans-memcached-works' do
   it returns the expected version; (3) it can --enable-ssl; and 
   '
   
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
   
+  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
   describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} memcached --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
